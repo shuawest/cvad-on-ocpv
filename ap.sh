4d0b7ccbcddfa91a7c517e87399db8de0a3a04a4
Josh West <shuawest@gmail.com>
2022-11-26 21:41:34 -0500

    moved scripts to root dir

--- ap.sh
+++ ap.sh
@@ -6,7 +6,7 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 . $SCRIPT_DIR/env-defaults.sh
 
-echo "ansible-playbook -i inventories/hosts-${GUID}.ini --extra-vars \"secrets_file=${SCRIPT_DIR}/../env-${GUID}.yaml\" $@"
+echo "ansible-playbook -i bootstrap/inventories/hosts-${GUID}.ini --extra-vars \"secrets_file=${SCRIPT_DIR}/env-${GUID}.yaml\" $@"
 
-ansible-playbook -i inventories/hosts-${GUID}.ini --extra-vars "secrets_file=${SCRIPT_DIR}/../env-${GUID}.yaml" $@
+ansible-playbook -i bootstrap/inventories/hosts-${GUID}.ini --extra-vars "secrets_file=${SCRIPT_DIR}/env-${GUID}.yaml" $@
 
