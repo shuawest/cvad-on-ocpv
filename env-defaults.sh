c78e06e78ea4f1d733ba0b34724c8dda4666b3e2
Josh West <shuawest@gmail.com>
2022-12-02 18:21:31 -0500

    Delete env-defaults.sh

--- env-defaults.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-export GUID=c8rpw
-export BASTION_PASS=Op9wJHInV9Ar
-export BASTION_HOST=provision.c8rpw.dynamic.opentlc.com
-export BASTION_USER=c8rpw-user
-export KUBEADMIN_PASS=x4bw3-VU5qX-jmrA6-bN53R 
